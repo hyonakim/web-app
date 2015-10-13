0e785b0cbf9058035e5747ad0d9aa1e18cbe7cfb
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2015-10-13 16:03:46 -0400

    main.js push ...need to work on

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,22 @@
 
 (function () {
 
-  console.log('It Works!');
+  var formUrl = 'http://json-data.herokuapp.com/forms';
+  var formtemplate = function formtemplate(data) {
+    var template = '\n<div class =\'container\'><input type=\'' + data.text + '\' placeholder= \'' + data.label + '\' id=\'' + data.id + '\' ></div>\n';
+  };
+
+  var form = $.ajax({
+    url: formUrl
+  }).then(function (response) {
+    _.each(response, function (item) {
+      var itemHTML = formtemplate(item);
+      if (item.type === "text" || item.type === 'tel' || item.type === 'email') {
+        console.log(itemHTML);
+        $('.container').append(itemHTML);
+      }
+    });
+  });
+
+  // _.each(response.object,function)
 })();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
 (function () {
 
-  console.log('It Works!');
+
+ let formUrl      ='http://json-data.herokuapp.com/forms';
+ let formtemplate = function(data){
+let template = `
+<div class ='firstName'><input type="${data.text}" placeholder="${data.label}" id="${data.id}"></div>
+<div class ='lastName'><input type='text' value= ${data.id}></div>
+<div class='email'></div>
+<div class='website'></div> 
+<div class='lang'></div> 
+<div class='comment'></div> 
+<div class='mobno'></div> 
+<div class='homeno'></div> 
+<button class='submit'></div> 
+`;
+$('.container').append(template);
+}
+
+
+var form = $.ajax(
+url :formUrl,
+datatype:'jsonp', 
+method  :'GET'
+).then(function(response){
+_.each(response, function(item){
+if(item.type ==='text' || item.type ==='tel' || item.type='email'){
+  console.log(item);
+}
+
+})
+});
+
+// _.each(response.object,function)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }());
