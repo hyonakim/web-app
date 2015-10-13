252435e76f8416fb2980f130703d563851281241
Hyona Kim <tekkieninja@gmail.com>
2015-10-13 18:14:32 -0400

    Edited the JS file, still working on it

--- app/js/main.js
+++ app/js/main.js
@@ -3,21 +3,23 @@
 (function () {
 
   var formUrl = 'http://json-data.herokuapp.com/forms';
+
   var formtemplate = function formtemplate(data) {
-    var template = '\n<div class =\'container\'><input type=\'' + data.text + '\' placeholder= \'' + data.label + '\' id=\'' + data.id + '\' ></div>\n';
+    var template = '\n// <div class =\'firstName\'><input type="' + data.type + '" placeholder="' + data.label + '" id="' + data.id + '"></div>\n\n<div class = \'dataInfo\'>\n<input type=\'' + data.type + '\' placeholder=\'{data.label}\' id=\'' + data.id + '\'>\n<button class=\'submit\'>Submit Form</button>\n</div> \n';
+    $('.container').append(template);
   };
 
   var form = $.ajax({
-    url: formUrl
-  }).then(function (response) {
+    url: formUrl,
+    dataType: 'json',
+    method: 'GET'
+  }).then((function (response) {
     _.each(response, function (item) {
-      var itemHTML = formtemplate(item);
-      if (item.type === "text" || item.type === 'tel' || item.type === 'email') {
-        console.log(itemHTML);
-        $('.container').append(itemHTML);
+      if (item.type === 'text' || item.type === 'tel' || item.type === 'email') {
+        console.log(item);
       }
     });
-  });
+  })());
 
   // _.each(response.object,function)
 })();
--- src/main.js
+++ src/main.js
@@ -2,36 +2,35 @@
 
 
  let formUrl      ='http://json-data.herokuapp.com/forms';
+
  let formtemplate = function(data){
 let template = `
-<div class ='firstName'><input type="${data.text}" placeholder="${data.label}" id="${data.id}"></div>
-<div class ='lastName'><input type='text' value= ${data.id}></div>
-<div class='email'></div>
-<div class='website'></div> 
-<div class='lang'></div> 
-<div class='comment'></div> 
-<div class='mobno'></div> 
-<div class='homeno'></div> 
-<button class='submit'></div> 
+// <div class ='firstName'><input type="${data.type}" placeholder="${data.label}" id="${data.id}"></div>
+
+<div class = 'dataInfo'>
+<input type='${data.type}' placeholder='{data.label}' id='${data.id}'>
+<button class='submit'>Submit Form</button>
+</div> 
 `;
 $('.container').append(template);
 }
 
 
-var form = $.ajax(
+var form = $.ajax({
 url :formUrl,
-datatype:'jsonp', 
+dataType:'json', 
 method  :'GET'
-).then(function(response){
+}).then(function(response){
 _.each(response, function(item){
-if(item.type ==='text' || item.type ==='tel' || item.type='email'){
+if(item.type ==='text' || item.type ==='tel' || item.type === 'email'){
   console.log(item);
 }
 
 })
-});
+}());
 
 // _.each(response.object,function)
+
 
 
 
