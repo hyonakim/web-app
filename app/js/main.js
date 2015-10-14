c541a218c7dafc81c6dad5fe9d0d7c3005008dbb
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2015-10-13 21:00:34 -0400

    added select using js

--- app/js/main.js
+++ app/js/main.js
@@ -3,21 +3,67 @@
 (function () {
 
   var formUrl = 'http://json-data.herokuapp.com/forms';
-  var formtemplate = function formtemplate(data) {
-    var template = '\n<div class =\'container\'><input type=\'' + data.text + '\' placeholder= \'' + data.label + '\' id=\'' + data.id + '\' ></div>\n';
+
+  var promise = $.getJSON(formUrl);
+
+  promise.then(function (response) {
+
+    iterate(response);
+  });
+
+  var iterate = function iterate(response_arr) {
+
+    _.each(response_arr, function (item) {
+      if (item.type === 'text' || item.type === 'tel' || item.type === 'email') {
+        var htmldata = formtemplate(item);
+      } else if (item.type === "textarea") {} else if (item.type === 'select') {
+        var htmldata = formtemplate2(item);
+      } else if (item.type === "textarea") {
+
+        var htmldata = formtemplate1(item);
+      }
+
+      $('.container').append(htmldata);
+    });
   };
 
-  var form = $.ajax({
-    url: formUrl
-  }).then(function (response) {
-    _.each(response, function (item) {
-      var itemHTML = formtemplate(item);
-      if (item.type === "text" || item.type === 'tel' || item.type === 'email') {
-        console.log(itemHTML);
-        $('.container').append(itemHTML);
-      }
-    });
-  });
+  var formtemplate = function formtemplate(data) {
+    var template = '\n          <div class="input-data" >\n          <input class="input-fields" type="' + data.type + '" placeholder="' + data.label + '" id="' + data.id + '">\n          </div>';
+    return template;
+  };
+
+  var formtemplate1 = function formtemplate1(data) {
+    var template1 = '\n<div class="text-area" >\n          <input width ="200" type="' + data.type + '" placeholder="' + data.label + '" id="' + data.id + '">\n          \n          </div>';
+    return template1;
+  };
+
+  var formtemplate2 = function formtemplate2(data) {
+
+    var template2 = '\n<div class="lang-opt" >\n          <select class="sel-opt">\n          <option class ="lang" value="' + data.value + '">' + data.label + '</option>\n          </select>\n          </div>';
+
+    return template2;
+  };
+
+  //  let formUrl      ='http://json-data.herokuapp.com/forms';
+  //  let formtemplate = function(data){
+  // let template = `
+  // <div class ='firstName'><input type="${data.text}" placeholder="${data.label}" id="${data.id}"></div>
+  // <div class ='lastName'><input type='text' value= ${data.id}></div>
+  // `;
+  // $('.container').append(template);
+  // }
+
+  // var form = $.ajax(
+  // url :formUrl,
+  // datatype:'json',
+  // method  :'GET'
+  // ).then(function(response){
+  // _.each(response, function(item){
+  // if(item.type ==='text' || item.type ==='tel' || item.type==='email'){
+  //   console.log(item);
+  // }
+
+  // })
 
   // _.each(response.object,function)
 })();
--- src/main.js
+++ src/main.js
@@ -1,35 +1,93 @@
 (function () {
 
+let formUrl      ='http://json-data.herokuapp.com/forms';
 
- let formUrl      ='http://json-data.herokuapp.com/forms';
- let formtemplate = function(data){
-let template = `
-<div class ='firstName'><input type="${data.text}" placeholder="${data.label}" id="${data.id}"></div>
-<div class ='lastName'><input type='text' value= ${data.id}></div>
-<div class='email'></div>
-<div class='website'></div> 
-<div class='lang'></div> 
-<div class='comment'></div> 
-<div class='mobno'></div> 
-<div class='homeno'></div> 
-<button class='submit'></div> 
-`;
-$('.container').append(template);
-}
+var promise = $.getJSON(formUrl);
 
+promise.then(function(response){
 
-var form = $.ajax(
-url :formUrl,
-datatype:'jsonp', 
-method  :'GET'
-).then(function(response){
-_.each(response, function(item){
-if(item.type ==='text' || item.type ==='tel' || item.type='email'){
-  console.log(item);
-}
+iterate(response);
 
-})
 });
+
+var iterate=function(response_arr){
+
+  _.each(response_arr, function(item){
+    if(item.type==='text' ||item.type==='tel'||item.type==='email'){
+    var htmldata= formtemplate(item);
+  }else if(item.type==="textarea"){
+
+  }
+  else if(item.type==='select'){
+    var htmldata=formtemplate2(item);
+  }
+   else if(item.type==="textarea"){
+
+  var htmldata=formtemplate1(item)
+   }
+
+   $('.container').append(htmldata);
+
+
+    });
+};
+
+
+
+let formtemplate = function(data){
+          let template = `
+          <div class="input-data" >
+          <input class="input-fields" type="${data.type}" placeholder="${data.label}" id="${data.id}">
+          </div>`;
+          return template;
+        }
+
+
+let formtemplate1 = function(data){
+let template1 =`
+<div class="text-area" >
+          <input width ="200" type="${data.type}" placeholder="${data.label}" id="${data.id}">
+          
+          </div>`;  
+        return template1;
+ }    
+
+let formtemplate2 = function(data){
+
+let template2 =`
+<div class="lang-opt" >
+          <select class="sel-opt">
+          <option class ="lang" value="${data.value}">${data.label}</option>
+          </select>
+          </div>`;
+
+return template2;
+
+}    
+
+
+
+//  let formUrl      ='http://json-data.herokuapp.com/forms';
+//  let formtemplate = function(data){
+// let template = `
+// <div class ='firstName'><input type="${data.text}" placeholder="${data.label}" id="${data.id}"></div>
+// <div class ='lastName'><input type='text' value= ${data.id}></div> 
+// `;
+// $('.container').append(template);
+// }
+
+
+// var form = $.ajax(
+// url :formUrl,
+// datatype:'json', 
+// method  :'GET'
+// ).then(function(response){
+// _.each(response, function(item){
+// if(item.type ==='text' || item.type ==='tel' || item.type==='email'){
+//   console.log(item);
+// }
+
+// })
 
 // _.each(response.object,function)
 
